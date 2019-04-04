359881543e54029dd38ac73f9cdb9f0eaa76f5c2
Øyvind Gujord <oyvind_gujord@hotmail.com>
2019-04-04 15:46:44 +0200

    build

--- src/components/Packages/PackagesComponent.jsx
+++ src/components/Packages/PackagesComponent.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-export default class Packagespage extends React.Component{
+export default class PackagesComponent extends React.Component{
 	
 	render(){
+		let app = this;
+		
 		return(
-			<div class="[ col-md-4 d-flex flex-column mb-4 ]">
-				<div class="col-about">
-					<img class="img-responsive" src={app.props.image} alt="placeholder image"/>
+			<div className="[ col-md-4 d-flex flex-column mb-4 ]">
+				<div className="[ col-about ]">
+					<img className="[ img-fluid ]" src={app.props.image} alt={app.props.destination}/>
 					<h1>{app.props.destination}</h1>
-					<p>{app.props.content}</p>
-					<button type="button" class="btn btn-primary col-about__btn">View More</button>
+					<p>{app.props.description}</p>
+					<button type="button" className="btn btn-primary col-about__btn">View More</button>
 				</div>
 			</div>
 		);
--- src/pages/packagespage/packagespage.jsx
+++ src/pages/packagespage/packagespage.jsx
@@ -1,26 +1,56 @@
 import React from 'react';
 import CarouselComponent from './../../components/Carousel/CarouselComponent';
+import PackagesComponent from './../../components/Packages/PackagesComponent';
 import { packages } from './../../components/Packages/PackageDestinationComponent';
 
 export default class Packagespage extends React.Component{
 	constructor(props){
 		super(props);
+		
+		this.state = {
+			packages: [],
+			allPackages: []
+		}
 	}
 	
+	componentDidMount(){
+		let app = this;
+		app.getPackages();
+	}
+	
+	getPackages(){
+		let app = this;
+		app.setState({
+			packages: packages
+		});
+	}
 	
 	createPackages(){
-		console.log(packages);
+		let app = this;
+		let allPackages = app.state.packages;
+		
+		allPackages.forEach((value, key)=>{
+			app.state.allPackages.push(
+				<PackagesComponent
+					image = {value.image}
+					destination = {value.destination}
+					description = {value.description}
+					key = {key}
+				/>
+			);
+		});
+		
 	}
 	
 	render(){
 		let app = this;
-		
 		app.createPackages();
+		
 		return(
 			<div>
 				<CarouselComponent/>
 				<div className="[ row ]">
-					hello world
+					{app.state.allPackages}
 				</div>
 			</div>
 		);
