a9864ed91064dd9140782a7db4087c8fd3f9fe30
Øyvind Gujord <oyvind_gujord@hotmail.com>
2019-04-05 16:22:34 +0200

    added styling

--- src/components/Homepage/HomepageComponent.jsx
+++ src/components/Homepage/HomepageComponent.jsx
@@ -13,33 +13,41 @@ export default class HomepageComponent extends React.Component{
 				<Row className="[ row-eq-height ]">
 					<Col md="8" className="[ d-flex flex-column mb-4 ]">
 						<div className="[ col-home ]">
-							<h1 className="[ col-home__header ]">Destinations</h1>
+							<h1 className="[ col-home__header ]">Popular destinations</h1>
 
-							<Row className="[ row-eq-height ]">
+							<Row className="[ row-eq-height homepageContainer__row ]">
 								<Col md="6" className="[ mb-4 ]">
 									<a href="#/packages">
-										<img className="[ img-fluid ]" src={require("./../../img/photos/destinations/hawaii.jpg")} alt="Hawaii"/>
-										<span className="[ col-home__img-text ]">Hawaii</span>
+										<div className="[ col-home__overlay ]">
+											<img className="[ img-fluid ]" src={require("./../../img/photos/destinations/hawaii.jpg")} 	alt="Hawaii"/>
+											<span className="[ col-home__img-text ]">Hawaii</span>
+										</div>
 									</a>
 								</Col>
-								<Col md="6">
-									<a href="#/packages">
-										<img className="[ img-fluid ]" src={require("./../../img/photos/destinations/bali.jpg")} alt="Bali"/>
-										<span className="[ col-home__img-text ]">Bali</span>
-									</a>
-								</Col>
-								
-								<Col md="6">
-									<a href="#/packages">
-										<img className="[ img-fluid ]" src={require("./../../img/photos/destinations/mykonos.jpg")} alt="Bora Bora"/>
-										<span className="[ col-home__img-text ]">Mykonos</span>
+								<Col md="6" className="[ mb-4 ]">
+									<a href="#/packages" >
+										<div className="[ col-home__overlay ]">
+											<img className="[ img-fluid ]" src={require("./../../img/photos/destinations/mykonos.jpg")} alt="bali"/>
+											<span className="[ col-home__img-text ]">Mykonos</span>
+										</div>
 									</a>
 								</Col>
 								
-								<Col md="6">
+								<Col md="6" className="[ mb-4 ]">
 									<a href="#/packages">
-										<img className="[ img-fluid ]" src={require("./../../img/photos/destinations/santorini.jpg")} alt="Santorini"/>
-										<span className="[ col-home__img-text ]">Santorini</span>
+										<div className="[ col-home__overlay ]">
+											<img className="[ img-fluid ]" src={require("./../../img/photos/destinations/mykonos.jpg")} alt="Mykonos"/>
+											<span className="[ col-home__img-text ]">Mykonos</span>
+										</div>
+									</a>
+								</Col>
+								
+								<Col md="6" className="[ mb-4 ]">
+									<a href="#/packages">
+										<div className="[ col-home__overlay ]">
+											<img className="[ img-fluid ]" src={require("./../../img/photos/destinations/santorini.jpg")} alt="Santorini"/>
+											<span className="[ col-home__img-text ]">Santorini</span>
+										</div>
 									</a>
 								</Col>
 							</Row>
@@ -49,6 +57,21 @@ export default class HomepageComponent extends React.Component{
 					<Col md="4" className="[ d-flex flex-column mb-4 ]">
 						<div className="[ col-home ]">
 							<h1 className="[ col-home__header ]">Reviews</h1>
+							<Row>
+								<Col md="12">
+									<h2 className="[ col-home__header-two ]">Great vacation</h2>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>
+										<span className="[ col-home--grey ]"><i>- James May</i></span>
+									</p><br/>
+								</Col>								<Col md="12">
+									<h2 className="[ col-home__header-two ]">Superb customer service</h2>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>
+										<span className="[ col-home--grey ]"><i>- Margareth Ways</i></span>
+									</p>
+								</Col>
+							</Row>
 						</div>
 					</Col>
 				</Row>
